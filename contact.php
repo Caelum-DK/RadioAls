3db5cedd79d76a4300b1dc8c28f8231d0b4d8373
Kristian Brouer Seedorff <seedorff.himself@gmail.com>
2012-12-03 23:18:26 +0100

    form styling

--- contact.php
+++ contact.php
@@ -49,7 +49,7 @@ if(isset($_POST['submitted'])) {
 		//If there is no error, send the email
 		if(!isset($hasError)) {
 
-			$emailTo = 'seedorff.himself@gmail.com';
+			$emailTo = 'kontakt@caelum.dk';
 			$subject = 'Kontaktformular, besked fra '.$name;
 			$sendCopy = trim($_POST['sendCopy']);
 			$body = "Navn: $name \n\nEmail: $email \n\nBesked: $comments";
@@ -84,58 +84,61 @@ if(isset($_POST['submitted'])) {
 
 			<div class="content-entry">
 
-				<?php if(isset($emailSent) && $emailSent == true) { ?>
+				<div class="contact_form">
 
-					<div class="thanks">
-						<h1>Tak, <?=$name;?></h1>
-						<p>Din besked er sendt. Du hører fra os hurtigst muligt!</p>
-					</div>
+					<?php if(isset($emailSent) && $emailSent == true) { ?>
 
-				<?php } else { ?>
+						<div class="thanks">
+							<h1>Tak, <?=$name;?></h1>
+							<p>Din besked er sendt. Du hører fra os hurtigst muligt!</p>
+						</div>
 
-					<?php if (have_posts()) : ?>
-					
-					<?php while (have_posts()) : the_post(); ?>
-						<h1><?php the_title(); ?></h1>
-						<?php the_content(); ?>
+					<?php } else { ?>
+
+						<?php if (have_posts()) : ?>
 						
-						<?php if(isset($hasError) || isset($captchaError)) { ?>
-							<p class="error">Fejl i de indtastede oplysninger!<p>
-						<?php } ?>
-					
-						<form action="<?php the_permalink(); ?>" id="contactForm" method="post">
-					
-							<ul class="forms">
-								<li><label for="contactName">Name</label>
-									<input type="text" name="contactName" id="contactName" value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>" class="requiredField" />
-									<?php if($nameError != '') { ?>
-										<span class="error"><?=$nameError;?></span> 
-									<?php } ?>
-								</li>
-								
-								<li><label for="email">Email</label>
-									<input type="text" name="email" id="email" value="<?php if(isset($_POST['email']))  echo $_POST['email'];?>" class="requiredField email" />
-									<?php if($emailError != '') { ?>
-										<span class="error"><?=$emailError;?></span>
-									<?php } ?>
-								</li>
-								
-								<li class="textarea"><label for="commentsText">Comments</label>
-									<textarea name="comments" id="commentsText" rows="20" cols="30" class="requiredField"><?php if(isset($_POST['comments'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['comments']); } else { echo $_POST['comments']; } } ?></textarea>
-									<?php if($commentError != '') { ?>
-										<span class="error"><?=$commentError;?></span> 
-									<?php } ?>
-								</li>
-								<li class="inline"><input type="checkbox" name="sendCopy" id="sendCopy" value="true"<?php if(isset($_POST['sendCopy']) && $_POST['sendCopy'] == true) echo ' checked="checked"'; ?> /><label for="sendCopy">Send a copy of this email to yourself</label></li>
-								<li class="screenReader"><label for="checking" class="screenReader">If you want to submit this form, do not enter anything in this field</label><input type="text" name="checking" id="checking" class="screenReader" value="<?php if(isset($_POST['checking']))  echo $_POST['checking'];?>" /></li>
-								<li class="buttons"><input type="hidden" name="submitted" id="submitted" value="true" /><button type="submit">Email me &raquo;</button></li>
-							</ul>
-						</form>
-					
-						<?php endwhile; ?>
-					<?php endif; ?>
-				<?php } ?>
-			
+						<?php while (have_posts()) : the_post(); ?>
+							<h3>mail: <a href="mailTo:radioals@radioals.dk" target="_blank" title="Send os en mail">radioals@radioals.dk »</a></h3>
+							<?php the_content(); ?>
+							
+							<?php if(isset($hasError) || isset($captchaError)) { ?>
+								<p class="error">Fejl i de indtastede oplysninger!<p>
+							<?php } ?>
+						
+							<form action="<?php the_permalink(); ?>" id="contactForm" method="post">
+						
+								<ul class="forms">
+									<li>
+										<input type="text" class="input-text" name="contactName" id="contactName" placeholder="Navn" value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>" class="requiredField" />
+										<?php if($nameError != '') { ?>
+											<span class="error"><?=$nameError;?></span> 
+										<?php } ?>
+									</li>
+									
+									<li>
+										<input type="text" class="input-text" name="email" id="email" placeholder="Email" value="<?php if(isset($_POST['email']))  echo $_POST['email'];?>" class="requiredField email" />
+										<?php if($emailError != '') { ?>
+											<span class="error"><?=$emailError;?></span>
+										<?php } ?>
+									</li>
+									
+									<li class="textarea">
+										<textarea name="comments" id="commentsText" rows="20" cols="30" class="requiredField" placeholder="Besked"><?php if(isset($_POST['comments'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['comments']); } else { echo $_POST['comments']; } } ?></textarea>
+										<?php if($commentError != '') { ?>
+											<span class="error"><?=$commentError;?></span> 
+										<?php } ?>
+									</li>
+									
+									<li class="screenReader"><label for="checking" class="screenReader">If you want to submit this form, do not enter anything in this field</label><input type="text" name="checking" id="checking" class="screenReader" value="<?php if(isset($_POST['checking']))  echo $_POST['checking'];?>" /></li>
+									<li class="buttons"><input type="hidden" name="submitted" id="submitted" value="true" /><button type="submit">Send</button></li>
+									<div style="clear:both"></div>
+								</ul>
+							</form>
+						
+							<?php endwhile; ?>
+						<?php endif; ?>
+					<?php } ?>
+				</div>
 			</div>
 
 		</div>
--- header.php
+++ header.php
@@ -40,11 +40,6 @@
 		// End -->
 	</script>
 
-	<?php if( is_page('Kontakt') ){ ?>
-		<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/javascripts/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/javascripts/verif.js"></script>
-	<?php }?>
-
 </head>
 
 <body <?php body_class(); ?>>
